c417e5fda56a7635729fbcfb14ea60b838d2944d
Zach Molony <zachary.molony@ada.ac.uk>
2021-01-14 16:06:06 +0000

    created file structure

---
+++ Exercise 1/exercise 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream> //tell the compiler to add refererences for the standard devices cin, cout, clog, cerr; http://www.cplusplus.com/reference/iostream/
+using namespace std; // 'std::' is now implied
+
+// Exercise 1: ReverseWord
+
+string reverse(string word, size_t wordSize) { // take the given word and the word size
+  for (int i = 0; i < wordSize / 2; i++) { // iterate through half the characters
+      swap(word[i], word[wordSize - i - 1]); // swap these chars with their corresponding char on the other side of the word
+  }
+  return word; // return the reversed string
+}
+
+int main() {
+  string word;
+  cout << "Please enter a word: ";
+  cin >> word;
+  cout << word << " spelt backwards is: " << reverse(word, word.length()) << endl;
+  return 0;
+}
